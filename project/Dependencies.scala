3fb70e3ad51a28b2efb1306e0edf2558da5ecd77
Alexandros Bantis <abantis@tubi.tv>
2018-12-19 13:52:50 -0800

    Add Akka Dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,23 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+
+  object Version {
+
+    lazy val akkaCore = "2.5.19"
+    lazy val scalaTest = "3.0.5"
+  }
+
+  lazy val akka = Seq(
+    "com.typesafe.akka" %% "akka-actor" % Version.akkaCore,
+    "com.typesafe.akka" %% "akka-stream" % Version.akkaCore
+  )
+
+  lazy val testing = Seq(
+    "org.scalatest" %% "scalatest" % Version.scalaTest,
+    "com.typesafe.akka" %% "akka-testkit" % Version.akkaCore,
+    "com.typesafe.akka" %% "akka-stream-testkit" % Version.akkaCore
+  ).map(_ % Test)
+
+  lazy val all = akka ++ testing
 }
