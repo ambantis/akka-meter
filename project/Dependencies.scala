15c67a0515962449d128b5b564244e1a94e5e80c
Alexandros Bantis <abantis@tubi.tv>
2018-12-19 14:55:01 -0800

    Add Logback

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,11 +6,17 @@ object Dependencies {
 
     lazy val akkaCore = "2.5.19"
     lazy val scalaTest = "3.0.5"
+    lazy val logback = "1.2.3"
   }
 
   lazy val akka = Seq(
     "com.typesafe.akka" %% "akka-actor" % Version.akkaCore,
+    "com.typesafe.akka" %% "akka-slf4j" % Version.akkaCore,
     "com.typesafe.akka" %% "akka-stream" % Version.akkaCore
+  )
+
+  lazy val misc = Seq(
+    "ch.qos.logback" % "logback-classic" % Version.logback
   )
 
   lazy val testing = Seq(
@@ -19,5 +25,5 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-stream-testkit" % Version.akkaCore
   ).map(_ % Test)
 
-  lazy val all = akka ++ testing
+  lazy val all = akka ++ misc ++ testing
 }
